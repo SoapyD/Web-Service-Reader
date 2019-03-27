60e1a1750ce40a249913e600eb59993e5fbd2e42
SoapyD <tom_bombchild@hotmail.com>
2019-03-27 10:14:45 +0000

    slight alteration to save process call

--- _main.py
+++ _main.py
@@ -211,4 +211,4 @@ u_print('End: '+str(finish_time))
 u_print('Time Taken: '+str(finish_time - start_time))
 u_print('########################################')
 
-save_process(start_time, finish_time, str(finish_time - start_time), "Web-Service-Reader", error_count)
+save_process(start_time, finish_time, str(finish_time - start_time), "Web-Service-Reader")
