21a4f3e1c488b5595a8b55b83945ae8d8b90826c
SoapyD <tom_bombchild@hotmail.com>
2019-07-29 15:53:49 +0100

    turned update back on.

--- _main.py
+++ _main.py
@@ -82,4 +82,4 @@ u_print('End: '+str(finish_time))
 u_print('Time Taken: '+str(finish_time - start_time))
 u_print('########################################')
 
-#save_process(start_time, finish_time, str(finish_time - start_time), "Web-Service-Reader", 'hourly')
+save_process(start_time, finish_time, str(finish_time - start_time), "Web-Service-Reader", 'hourly')
