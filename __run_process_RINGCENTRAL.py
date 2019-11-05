8c762e74bd2c99098c62b1b5eb3dc15e542fbf3f
Thomas Cassady <thomas.cassady@littlefish.co.uk>
2019-11-05 11:04:50 +0000

    can only send dates to api so hand to amend the script to do so.

--- __run_process_RINGCENTRAL.py
+++ __run_process_RINGCENTRAL.py
@@ -15,12 +15,17 @@ time_unit = 7
 
 
 
-start_date = datetime.datetime(2019, 11, 4)
-end_date = datetime.datetime(2019, 11, 6)
-start_date = now.replace(microsecond=0) - datetime.timedelta(hours=2.0)
-end_date = now.replace(microsecond=0) + datetime.timedelta(hours=1.0)
+#start_date = datetime.datetime(2019, 11, 5)
+#end_date = datetime.datetime(2019, 11, 6)
 
-#CREATE TEMP WAREHOUSING TABLE NAMES HERE VIA A CLASS
+#CAN ONLY SEND DATES TO RINGCENTRAL, SO THE TIME COMPONENT NEEDS STRIPPING OUT
+start_date = now.replace(hour=0)
+start_date = start_date.replace(minute=0)
+start_date = start_date.replace(second=0)
+start_date = start_date.replace(microsecond=0)
+
+end_date = start_date + datetime.timedelta(days=1.0)
+
 
 
 ##############################################################################################################################################################
