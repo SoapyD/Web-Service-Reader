5ec3d8e64dad066a266eaedbfac268d682376842
Thomas Cassady <thomas.cassady@littlefish.co.uk>
2019-11-22 12:57:44 +0000

    small update

--- __run_process_TEST.py
+++ __run_process_TEST.py
@@ -30,7 +30,7 @@ end_date = now.replace(microsecond=0)
 ##############################################################################################################################
 ##############################################################################################################################
 
-""""""
+"""
 process_list = [
 	ws_process_class('HE','sys_user'),
 	ws_process_class('HE','incident_task'),
@@ -72,4 +72,39 @@ run_main("LFLIVEEXTRACT", process_list, start_date, end_date, time_type, time_un
 	delete_staging, print_internal, print_details)
 
 
+"""
 
+
+start_date = datetime.datetime(2019, 11, 11)
+end_date = datetime.datetime(2019, 11, 23)
+
+"""
+start_date = now.replace(hour=0)
+start_date = start_date.replace(minute=0)
+start_date = start_date.replace(second=0)
+start_date = start_date.replace(microsecond=0)
+
+end_date = start_date + datetime.timedelta(days=1.0)
+"""
+
+
+##############################################################################################################################################################
+##############################################################################################################################################################
+###############################################################################RINGCENTRAL
+##############################################################################################################################################################
+##############################################################################################################################################################
+
+
+process_list = [
+	ws_process_class('RINGCENTRAL','agents'),
+	ws_process_class('RINGCENTRAL','completedcontacts'),
+]
+
+
+#QUERY DATA AND MERGE IT INTO THE BASE TABLES AND TEMP WAREHOUSING TABLES
+run_main("RINGCENTRAL", process_list, start_date, end_date, time_type, time_unit, db, database, run_warehousing,
+	delete_staging, print_internal, print_details)
+
+"""
+generate_creation_query('RINGCENTRAL', 'completedcontacts')
+"""
